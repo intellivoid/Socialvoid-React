ad43257638e78961c21b6fda1ac0e6cf1e238f0a
Roj Serbest <roj@rojser.best>
2021-11-05 13:49:14 +0300

    Rm old loadin usage

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,14 +1,12 @@
 import { useRouter } from "next/router";
 import { logout } from "../client";
 import { runOnClient } from "../utils";
-import { setLoading } from "../stores";
 
 export default function Logout() {
   const router = useRouter();
 
   runOnClient(async () => {
     await logout();
-    setLoading(false);
     router.push("/");
   });
 
