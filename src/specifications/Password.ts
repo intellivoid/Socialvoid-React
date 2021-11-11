caa6b02f89470eb009a004260424ff8eacd76f7d
Roj Serbest <roj@rojser.best>
2021-11-11 14:21:03 +0300

    Update password specification

--- src/specifications/Password.ts
+++ src/specifications/Password.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 const Password = z
   .string({ required_error: "Password is required." })
-  .min(12, "The password should be 12 characters at least.")
+  // .min(12, "The password should be 12 characters at least.")
   .max(128, "The password can’t be longer than 128 characters.")
   .regex(/.*[A-Z].*/, "The password should contain a capital letter.")
   .regex(/.*[0-9].*[0-9].*/, "The password should contain 2 numbers at least.")
