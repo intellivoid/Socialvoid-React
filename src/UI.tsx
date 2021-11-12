22e7c0de47cfaf75bac7b81eb32382fb7633de94
Roj Serbest <roj@rojser.best>
2021-11-12 04:45:14 +0300

    Change import style

--- src/UI.tsx
+++ src/UI.tsx
@@ -1,19 +1,17 @@
-import {
-  Container,
-  CssBaseline,
-  ThemeProvider,
-  createTheme,
-  useMediaQuery,
-} from '@mui/material'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
+import createTheme from '@mui/material/styles/createTheme'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { SnackbarProvider } from 'notistack'
-import { useMemo } from 'react'
+import React from 'react'
 
 import Bar from './components/Bar'
 
 export default function UI({ children }: any) {
   const dark = useMediaQuery('(prefers-color-scheme: dark)')
 
-  const theme = useMemo(
+  const theme = React.useMemo(
     () => createTheme({ palette: { mode: dark ? 'dark' : 'light' } }),
     [dark]
   )
