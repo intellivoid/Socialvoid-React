f72dd87ef7f74ebf1d97c343c2aa1b2156469dcd
Roj Serbest <roj@rojser.best>
2021-11-12 13:58:28 +0300

    Finish the goal of the previous commit

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as B, Route as R, Routes as S } from 'react-router-dom'
 
+import Container from '@mui/material/Container'
+
+import Header from '../components/Header'
 import Home from './Home'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
@@ -7,13 +10,18 @@ import ToS from './ToS'
 
 export default function Routes() {
   return (
-    <B>
-      <S>
-        <R path="/" element={<Home />} />
-        <R path="/signin" element={<SignIn />} />
-        <R path="/signup" element={<SignUp />} />
-        <R path="/tos" element={<ToS />} />
-      </S>
-    </B>
+    <>
+      <Header />
+      <Container component="main" maxWidth="sm">
+        <B>
+          <S>
+            <R path="/" element={<Home />} />
+            <R path="/signin" element={<SignIn />} />
+            <R path="/signup" element={<SignUp />} />
+            <R path="/tos" element={<ToS />} />
+          </S>
+        </B>
+      </Container>
+    </>
   )
 }
