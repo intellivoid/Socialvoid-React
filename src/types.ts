8bcaf312fc9eee92239644c3b221227587d911bf
Roj Serbest <roj@rojser.best>
2021-11-12 14:11:39 +0300

    Improve NotDeletedPost type

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,12 @@ export interface RouteProps {
   query?: { [key: string]: string }
 }
 
-export type NotDeletedPost = {
-  [P in keyof Post]: NonNullable<Post[P]>
+export type NotDeletedPost = Post & {
+  peer: NonNullable<Post['peer']>
+  source: NonNullable<Post['source']>
+  text: NonNullable<Post['text']>
+  like_count: NonNullable<Post['like_count']>
+  repost_count: NonNullable<Post['repost_count']>
+  quote_count: NonNullable<Post['quote_count']>
+  reply_count: NonNullable<Post['reply_count']>
 }
